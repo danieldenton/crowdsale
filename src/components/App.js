760e237bab8b8f78f8ec22d8cd94cf897c1d30a2
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-05-03 13:12:35 -0700

    info ternary added

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import { ethers } from "ethers";
 
 import Navigation from "./Navigation";
+import Info from "./Info";
 
 export default function App() {
+  const [account, setAccount] = useState(null);
 
-    const loadlBockchainData = () => {
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        console.log(provider)
-    }
+  const loadlBockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+   
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
 
-    useEffect(() => {
-        loadlBockchainData()
-    })
+  useEffect(() => {
+    loadlBockchainData();
+  });
 
-    return (
-        <Container>
-            <Navigation />
-        </Container>
-    )
+  return (
+    <Container>
+      <Navigation />
+      <Info account={account}/>
+    </Container>
+  );
 }
