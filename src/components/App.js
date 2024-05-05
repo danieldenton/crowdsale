6807d3da7ade4cb218d38e4d617d165fc0e1064e
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-05-05 13:16:42 -0700

    everything is restored

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import { ethers } from "ethers";
+import * as ethers from "ethers";
 
 import Navigation from "./Navigation";
 import Info from "./Info";
@@ -8,8 +8,11 @@ import Info from "./Info";
 import TOKEN_ABI from "../abis/Token.json";
 import CROWDSALE_ABI from "../abis/Crowdsale.json";
 
+import config from "../config.json";
+
 export default function App() {
   const [provider, setProvider] = useState(null);
+  const [crowdsale, setCrowdsale] = useState(null);
   const [account, setAccount] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -17,23 +20,30 @@ export default function App() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(provider);
 
+    const { chainId } = await provider.getNetwork()
+
     const token = new ethers.Contract(
-      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      config[chainId].token.address,
       TOKEN_ABI,
       provider
     );
-    console.log(token.address)
     const crowdsale = new ethers.Contract(
-      "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      config[chainId].crowdsale.address,
       CROWDSALE_ABI,
       provider
     );
+    setCrowdsale(crowdsale);
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
     const account = ethers.utils.getAddress(accounts[0]);
     setAccount(account);
-    setLoading(false)
+    const accountBalance = await token.balanceOf(config[chainId].token.address);
+    const code = await provider.getCode(config[chainId].crowdsale.address)
+    console.log(code, chainId);
+  
+
+    setLoading(false);
   };
 
   useEffect(() => {
